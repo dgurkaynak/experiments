ef5765eec8d650e324f9cc14e130e648fa9cf0eb
Deniz Gurkaynak <dgurkaynak@Denizs-iMac.local>
2018-11-07 13:57:42 +0300

    09: Clean debugging stuff

--- src/09-text-physics/index.ts
+++ src/09-text-physics/index.ts
@@ -4,7 +4,7 @@ require('pathseg');
 
 import Two from 'two.js';
 import Matter from 'matter-js';
-import Experiment from '../experiment';
+import ExperimentTwoJs from '../experiment-twojs';
 import CanvasResizer from '../utils/canvas-resizer';
 import * as opentype from 'opentype.js';
 import Letter from './letter';
@@ -12,69 +12,63 @@ import Letter from './letter';
 import fontPath from './ModernSans-Light.otf';
 
 
-export default class TextPhysics extends Experiment {
-  // canvasResizer = new CanvasResizer({
-  //   dimension: 'fullscreen',
-  //   dimensionScaleFactor: 1
-  // });
+export default class TextPhysics extends ExperimentTwoJs {
+  canvasResizer = new CanvasResizer({
+    dimension: 'fullscreen'
+  });
   two: Two;
   letters: Letter[] = [];
 
   engine = Matter.Engine.create();
-  render = Matter.Render.create({
-    element: this.containerEl,
-    engine: this.engine
-  });
+  // render = Matter.Render.create({
+  //   element: this.containerEl,
+  //   engine: this.engine
+  // });
 
 
   constructor() {
     super();
 
-
     this.two = new Two({
       type: Two.Types.canvas,
-      width: 800,
-      height: 600,
+      width: this.canvasResizer.canvasWidth,
+      height: this.canvasResizer.canvasHeight,
       ratio: window.devicePixelRatio
     });
-    this.two.appendTo(this.containerEl);
   }
 
 
   async init() {
     const font = await loadFont(fontPath);
 
-    const refRect = this.two.makeRectangle(450, 400, 700, 400);
-    refRect.noFill();
-    refRect.stroke = 'rgba(0, 0, 255, 0.5)';
-    refRect.linewidth = 1;
-
-    // l (kucuk le) de sikinti var
     let left = 0;
-    ('VWXYZ').split('').forEach((char) => {
+    ('HAPPY?').split('').forEach((char) => {
       const letter = new Letter(font, this.two, char, 144, 100 + left, 200);;
       Matter.World.add(this.engine.world, letter.body);
 
       letter.view.fill = 'black';
-      // letter.view.noStroke();
+      letter.view.noStroke();
 
-      const rect = this.two.makeRectangle(0, 0, letter.pathWidth, letter.pathHeight);
-      rect.fill = 'rgba(0, 0, 0, 0)';
-      rect.stroke = 'rgba(0, 255, 0, 0.5)';
-      rect.linewidth = 1;
-      letter.view.add(rect);
+      // Draw bounding box for debug purposes
+      // const rect = this.two.makeRectangle(0, 0, letter.pathWidth, letter.pathHeight);
+      // rect.fill = 'rgba(0, 0, 0, 0)';
+      // rect.stroke = 'rgba(0, 255, 0, 0.5)';
+      // rect.linewidth = 1;
+      // letter.view.add(rect);
 
       this.letters.push(letter);
 
-      left += letter.pathWidth;
+      left += letter.pathWidth + 10;
     });
 
-    const ground = Matter.Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+    const ground = Matter.Bodies.rectangle(
+      this.canvasResizer.canvasWidth / 2, 
+      this.canvasResizer.canvasHeight + 24, 
+      this.canvasResizer.canvasWidth, 
+      50, 
+      { isStatic: true }
+    );
     Matter.World.add(this.engine.world, ground);
-
-    const groundView = this.two.makeRectangle(400, 610, 810, 60);
-    groundView.fill = 'rgba(255, 0, 0, 0.25)';
-    groundView.noStroke();
 
     this.two.update();
 
@@ -84,7 +78,7 @@ export default class TextPhysics extends Experiment {
 
   requestAnimationFrame() {
     Matter.Engine.update(this.engine, 1000 / 60);
-    Matter.Render.run(this.render);
+    // Matter.Render.run(this.render);
 
     this.letters.forEach(x => x.update());
     this.two.update();
