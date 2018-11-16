b2bcdb8a21358f98ae18ccc1729b9216f49dabbf
Deniz Gurkaynak <dgurkaynak@Denizs-iMac.local>
2018-11-16 07:17:24 +0300

    Remove experiment base classes and loader

--- src/experiment.ts
+++
@@ -1,8 +0,0 @@
-export default class Experiment {
-  containerEl = document.getElementById('container');
-  statsEl = document.getElementById('stats');
-  requestAnimationFrame: Function;
-
-  async init() {}
-  async destroy() {}
-}
