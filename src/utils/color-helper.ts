ff0b21af698e791ef53982b47f75ec449772bca7
Deniz Gurkaynak <dgurkaynak@gmail.com>
2019-02-04 20:04:20 +0300

    color-helper: hex2rgb

--- src/utils/color-helper.ts
+++ src/utils/color-helper.ts
@@ -10,7 +10,6 @@
  * @returns {String}
  */
 export function lerp(a, b, amount) {
-
   // var ah = parseInt(a.replace(/#/g, ''), 16),
   var ah = +a.replace('#', '0x'),
       ar = ah >> 16, ag = ah >> 8 & 0xff, ab = ah & 0xff,
@@ -22,4 +21,28 @@ export function lerp(a, b, amount) {
       rb = ab + amount * (bb - ab);
 
   return '#' + ((1 << 24) + (rr << 16) + (rg << 8) + rb | 0).toString(16).slice(1);
+}
+
+
+/**
+ * Converts hex to rgb.
+ * Supports shorthand `#fff` colors.
+ * @param {String} hex
+ * @example
+ * // returns { r: 255, g: 255, b: 255 }
+ * hex2rgb('#fff')
+ */
+export function hex2rgb(hex: string) {
+  // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+  var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+  hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+      return r + r + g + g + b + b;
+  });
+
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? {
+      r: parseInt(result[1], 16),
+      g: parseInt(result[2], 16),
+      b: parseInt(result[3], 16)
+  } : null;
 }
