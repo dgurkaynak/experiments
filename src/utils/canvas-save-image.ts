dfcaefb2740ea3c855946abd53ec15f283c06b55
Deniz Gurkaynak <dgurkaynak@gmail.com>
2019-02-05 17:32:11 +0300

    canvas-save-image: Fix saving big canvases

--- src/utils/canvas-save-image.ts
+++ src/utils/canvas-save-image.ts
@@ -1,7 +1,20 @@
+// https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/toBlob#Polyfill
+function dataURIToBlob(dataURI: string) {
+  const binStr = atob(dataURI.split(',')[1]);
+  const arr = new Uint8Array(binStr.length);
+
+  for (let i = 0; i < binStr.length; i++) {
+    arr[i] = binStr.charCodeAt(i);
+  }
+
+  return new Blob([arr]);
+}
+
 export default function saveImage(canvas: HTMLCanvasElement) {
   const dataString = canvas.toDataURL('image/png');
+  const blob = dataURIToBlob(dataString);
   const link = document.createElement('a');
-  link.download = 'image.png';
-  link.href = dataString;
+  link.href = URL.createObjectURL(blob);
+  link.download = `image-${Date.now()}.png`;
   link.click();
 }
