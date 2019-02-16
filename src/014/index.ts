02084e369b8021d9d9c2b0cf8dc1c2fdcae6c360
Deniz Gurkaynak <dgurkaynak@Denizs-iMac.local>
2019-02-16 21:49:14 +0300

    14: Images, images and more images....

--- src/014/index.ts
+++ src/014/index.ts
@@ -7,9 +7,57 @@ import FaceDeformer from './face-deformer';
 import PoissonBlender from './poisson-blender';
 
 
-import imagePath from './assets/friends.jpg';
-// import imagePath from './assets/himym.jpeg';
-import faceImagePath from './assets/IMG_0637.JPG';
+import sourceImagePath from './assets/IMG_0637.JPG';
+import imagePath1 from './assets/friends1.jpg';
+import imagePath2 from './assets/friends2.jpg';
+import imagePath3 from './assets/friends3.jpg';
+import imagePath4 from './assets/himym1.jpeg';
+import imagePath12 from './assets/himym2.jpg';
+import imagePath5 from './assets/seinfeld1.png';
+import imagePath6 from './assets/seinfeld2.jpg';
+import imagePath7 from './assets/seinfeld3.jpg';
+import imagePath8 from './assets/shannon.jpg';
+import imagePath9 from './assets/ali-ihsan-varol1.jpg';
+import imagePath10 from './assets/ali-ihsan-varol2.jpg';
+import imagePath11 from './assets/ali-ihsan-varol3.jpg';
+import imagePath13 from './assets/kratos.jpg';
+import imagePath14 from './assets/bsg1.jpg';
+import imagePath15 from './assets/bsg2.jpg';
+import imagePath16 from './assets/bsg3.jpg';
+import imagePath17 from './assets/acdc1.jpg';
+import imagePath18 from './assets/acdc2.jpg';
+import imagePath19 from './assets/gnr1.jpeg';
+import imagePath20 from './assets/gnr2.jpg';
+import imagePath21 from './assets/gnr3.jpeg';
+import imagePath22 from './assets/keanu1.jpg';
+import imagePath23 from './assets/keanu2.jpg';
+import imagePath24 from './assets/keanu3.jpg';
+const imagePaths = [
+  imagePath1,
+  imagePath2,
+  imagePath3,
+  imagePath4,
+  imagePath12,
+  imagePath5,
+  imagePath6,
+  imagePath7,
+  imagePath8,
+  imagePath9,
+  imagePath10,
+  imagePath11,
+  imagePath13,
+  imagePath14,
+  imagePath15,
+  imagePath16,
+  imagePath17,
+  imagePath18,
+  imagePath19,
+  imagePath20,
+  imagePath21,
+  imagePath22,
+  imagePath23,
+  imagePath24,
+].reverse();
 
 import ssdMobileNetV1Manifest from './faceapi_weights/ssd_mobilenetv1_model-weights_manifest.json';
 import ssdMobileNetV1ModelPath1 from './faceapi_weights/ssd_mobilenetv1_model-shard1.weights';
@@ -67,7 +115,7 @@ async function main() {
   await sleep(500);
 
   // Source face
-  const face = await getSourceFace(faceImagePath);
+  const face = await getSourceFace(sourceImagePath);
   const deformer = new FaceDeformer(
     readImageData(face.image),
     face.landmarks.points,
@@ -75,7 +123,7 @@ async function main() {
     3000 // Maximum input height
   );
 
-  const imagePaths = [imagePath];
+  // Swap faces and print results
   for (const imagePath of imagePaths) {
     const { inputImage } = await swapFaces(imagePath, deformer);
 
