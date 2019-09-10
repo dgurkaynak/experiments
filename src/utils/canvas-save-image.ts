110b402afc16cb79699b06248a8ca6dbec7b3501
Deniz Gurkaynak <dgurkaynak@gmail.com>
2019-09-10 08:33:57 +0300

    canvas-save-image: A little documentation

--- src/utils/canvas-save-image.ts
+++ src/utils/canvas-save-image.ts
@@ -1,7 +1,13 @@
-import { dataURIToBlob } from './utils';
 import { toBlobAsync } from './canvas-helper';
 
 
+/**
+ * Downloads canvas' current state as PNG image. Image dimensions will be canvas's `width` and `height` attr.
+ * So if you want your image to be at specific dimensions, you better set `CanvasResizer`s `dimension` option.
+ *
+ * NOTE: This does not work with `THREE.WebGLRenderer`s default options. You need to set
+ * `preserveDrawingBuffer: true` option. Otherwise, the image will be black.
+ */
 export default async function saveImage(canvas: HTMLCanvasElement) {
   const blob = await toBlobAsync(canvas);
   const link = document.createElement('a');
