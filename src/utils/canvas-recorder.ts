3b8d2add3d72b75bb2b0061253e36494b84d556e
Deniz Gurkaynak <dgurkaynak@gmail.com>
2019-09-10 18:24:21 +0300

    canvas-recorder: Some documentation

--- src/utils/canvas-recorder.ts
+++ src/utils/canvas-recorder.ts
@@ -1,6 +1,43 @@
 import CCapture from 'ccapture.js';
 
 
+/**
+ * Records the canvas keyframes to be combined into a video.
+ * Required 3 parameters:
+ *  - `canvas`: Canvas element
+ *  - `duration`: Desired video duration in ms
+ *  - `frameRate`: Desired video frame rate
+ *
+ * Example usage:
+ * ```js
+ * // We want to record `canvasEl` for 15 seconds in 30 fps
+ * const canvasRecorder = new CanvasRecorder(canvasEl, 15000, 30);
+ *
+ * // Just a notification for record ending
+ * canvasRecorder.onEnded = () => console.log('record ended');
+ *
+ * // In your draw or requestAnimationFrame function, call `capture()` method
+ * // Don't worry, we don't started capturing yet, so this is a no-op for now
+ * canvasRecorder.capture();
+ *
+ * // Then start your recording, you can trigger it by a button click:
+ * captureButton.addEventListener('click', () => {
+ *    canvasRecorder.start();
+ * }, false);
+ * ```
+ *
+ * After calling `start()` method, CanvasRecorder will capture every keyframe until
+ * desired number of keyframes are collected. All the keyframes are saved as PNG files
+ * that their dimension is exactly same with canvas element. (canvasEl.width and canvasEl.height)
+ *
+ * When capture is ended, you will see `capture ended` console message, because we did it so.
+ * A random tar file should be automatically downloaded when it ends.
+ *
+ * Extract it, go to that folder in terminal, and combine them with ffmpeg with this command:
+ * ```sh
+ * ffmpeg -r 30 -f image2 -s 1024x1024 -i "%07d.png" -vcodec libx264 -crf 0 -pix_fmt yuv420p output.mp4
+ * ```
+ */
 export default class CanvasRecorder {
   capturer: CCapture;
   onEnded = () => {};
