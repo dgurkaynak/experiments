f253e9d1463a8d76375a9789bd2dfceeb7f98bce
Deniz Gurkaynak <dgurkaynak@gmail.com>
2020-06-25 22:36:18 +0300

    refactor(6): migrate to raw js/html

--- src/6-wiggly-text/index.js
+++ src/6-wiggly-text/index.js
@@ -1,16 +1,16 @@
-import * as THREE from 'three';
-import Stats from 'stats.js';
-import * as dat from 'dat.gui';
-import OrbitControlsFactory from 'three-orbit-controls';
-const OrbitControls = OrbitControlsFactory(THREE);
-import CanvasResizer from '../utils/canvas-resizer';
-import Animator from '../utils/animator';
-import PerlinCanvas from '../utils/perlin-canvas';
-import colors from 'nice-color-palettes';
-import sampleSize from 'lodash/sampleSize';
+// Global deps:
+// - three
+// - stats.js
+// - dat.gui
+// - three-orbit-controls
+// - nice-color-palettes
+// - lodash/sampleSize
 
-import theBoldFontData from './the-bold-font.json';
+import { CanvasResizer } from '../lib/canvas-resizer.js';
+import { Animator } from '../lib/animator.js';
+import { PerlinCanvas } from '../lib/perlin-canvas.js';
 
+import theBoldFontData from './the-bold-font.js';
 
 /**
  * Constants
@@ -38,14 +38,13 @@ const GUISettings = class {
   xOffset = -1.65;
 
   randomizeColors = () => {
-    const randomTwoColors = sampleSize(sampleSize(colors, 1)[0], 2);
+    const randomTwoColors = _.sampleSize(_.sampleSize(niceColorPalettes100, 1)[0], 2);
     settings.bgColor = randomTwoColors[0];
     setBackgroundColor(settings.bgColor);
     settings.fontColor = randomTwoColors[1];
     setFontColor(settings.fontColor);
-  }
+  };
 };
-
 
 /**
  * Setup environment
@@ -54,27 +53,33 @@ const elements = {
   container: document.getElementById('container'),
   stats: document.getElementById('stats'),
 };
-const renderer = new THREE.WebGLRenderer({ antialias: window.devicePixelRatio == 1 });
+const renderer = new THREE.WebGLRenderer({
+  antialias: window.devicePixelRatio == 1,
+});
 const resizer = new CanvasResizer(renderer.domElement, {
   dimension: [1024, 1024],
-  dimensionScaleFactor: window.devicePixelRatio
+  dimensionScaleFactor: window.devicePixelRatio,
 });
 const animator = new Animator(animate);
 const stats = new Stats();
 const settings = new GUISettings();
 const gui = new dat.GUI();
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(35, resizer.width / resizer.height, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(
+  35,
+  resizer.width / resizer.height,
+  0.1,
+  1000
+);
 const orbitControls = ENABLE_ORBIT_CONTROLS ? new OrbitControls(camera) : null;
-
 
 /**
  * Experiment variables
  */
 const fontLoader = new THREE.FontLoader();
-let font: THREE.Font;
+let font;
 const perlinCanvas = new PerlinCanvas(128);
-let meshes: THREE.Mesh[] = [];
+let meshes = [];
 const texture = new THREE.CanvasTexture(perlinCanvas.canvas);
 texture.wrapS = THREE.MirroredRepeatWrapping;
 texture.wrapT = THREE.MirroredRepeatWrapping;
@@ -86,9 +91,8 @@ const material = new THREE.MeshStandardMaterial({
   displacementScale: 0.1,
   displacementBias: -0.05,
   metalness: 0.1,
-  roughness: 0.5
+  roughness: 0.5,
 });
-
 
 /**
  * Settings
@@ -96,16 +100,21 @@ const material = new THREE.MeshStandardMaterial({
 gui.close();
 
 const viewSettings = gui.addFolder('View');
-viewSettings.addColor(settings, 'bgColor').listen().onChange(setBackgroundColor);
+viewSettings
+  .addColor(settings, 'bgColor')
+  .listen()
+  .onChange(setBackgroundColor);
 viewSettings.addColor(settings, 'fontColor').listen().onChange(setFontColor);
 viewSettings.add(settings, 'randomizeColors');
 
 const textSettings = gui.addFolder('Text');
-let lineTextControllers: dat.GUIController[] = [];
-textSettings.add(settings, 'lineCount', 1, 10).step(1).onFinishChange(setupScene);
+let lineTextControllers = [];
+textSettings
+  .add(settings, 'lineCount', 1, 10)
+  .step(1)
+  .onFinishChange(setupScene);
 textSettings.add(settings, 'fontSize', 0.5, 2).step(0.01).onChange(setupScene);
 textSettings.add(settings, 'xOffset', -5, 5).step(0.01).onChange(setupScene);
-
 
 /**
  * Main/Setup function, initialize stuff...
@@ -147,7 +156,6 @@ async function main() {
   animator.start();
 }
 
-
 function setupScene() {
   // Remove the old ones
   meshes.forEach((mesh) => {
@@ -164,28 +172,30 @@ function setupScene() {
 
   // Add new ones
   for (let i = 0; i < settings.lineCount; i++) {
-    const text = settings['lineText' + (i+1)];
+    const text = settings['lineText' + (i + 1)];
     const geometry = new THREE.TextGeometry(text, {
       font: font,
       size: settings.fontSize,
       height: settings.fontSize * 0.75,
-      curveSegments: 12
+      curveSegments: 12,
     });
 
     const mesh = new THREE.Mesh(geometry, material);
     mesh.castShadow = true;
     mesh.receiveShadow = true;
     mesh.position.x = settings.xOffset;
-    mesh.position.y = -(settings.fontSize * 1.1) * (i + 1 - (settings.lineCount / 2));
+    mesh.position.y =
+      -(settings.fontSize * 1.1) * (i + 1 - settings.lineCount / 2);
     scene.add(mesh);
     meshes.push(mesh);
 
     // Add new dat.gui
-    const controller = textSettings.add(settings, 'lineText' + (i+1)).onFinishChange(setupScene);
+    const controller = textSettings
+      .add(settings, 'lineText' + (i + 1))
+      .onFinishChange(setupScene);
     lineTextControllers.push(controller);
   }
 }
-
 
 /**
  * Animate stuff...
@@ -196,33 +206,29 @@ function animate() {
 
   perlinCanvas.draw();
   meshes.forEach((mesh) => {
-    (<THREE.MeshStandardMaterial>mesh.material).displacementMap.needsUpdate = true;
+    mesh.material.displacementMap.needsUpdate = true;
   });
   renderer.render(scene, camera);
 
   if (ENABLE_STATS) stats.end();
 }
 
-
-function setBackgroundColor(color: string) {
+function setBackgroundColor(color) {
   renderer.setClearColor(color);
 }
 
-
-function setFontColor(color: string) {
+function setFontColor(color) {
   material.color = new THREE.Color(color);
 }
-
 
 /**
  * On window resized
  */
-function onWindowResize(width: number, height: number) {
+function onWindowResize(width, height) {
   camera.aspect = width / height;
   camera.updateProjectionMatrix();
   renderer.setSize(width, height);
 }
-
 
 /**
  * Clean your shit
@@ -234,15 +240,15 @@ function dispose() {
   orbitControls && orbitControls.dispose();
   meshes.forEach((mesh) => {
     mesh.geometry.dispose();
-    (<THREE.MeshStandardMaterial>mesh.material).dispose();
+    mesh.material.dispose();
   });
 
   Object.keys(elements).forEach((key) => {
     const element = elements[key];
-    while (element.firstChild) { element.removeChild(element.firstChild); }
+    while (element.firstChild) {
+      element.removeChild(element.firstChild);
+    }
   });
 }
 
-
-main().catch(err => console.error(err));
-(module as any).hot && (module as any).hot.dispose(dispose);
+main().catch((err) => console.error(err));
---
+++ src/lib/perlin-canvas.js
@@ -0,0 +1,50 @@
+// Dependencies
+// - noise.js
+
+export class PerlinCanvas {
+  canvas = document.createElement('canvas');
+  ctx = this.canvas.getContext('2d');
+  image = null;
+  data = null;
+  height = 0;
+  noiseDivider = 50;
+
+  constructor(size) {
+    this.size = size;
+    this.canvas.width = size;
+    this.canvas.height = size;
+    this.image = this.ctx.createImageData(size, size);
+    this.data = this.image.data;
+  }
+
+  draw() {
+    // Cache width and height values for the canvas.
+    const cWidth = this.canvas.width;
+    const cHeight = this.canvas.height;
+    let max = -Infinity;
+    let min = Infinity;
+    const noiseFn = noise.perlin3; // noise.simplex3;
+
+    for (let x = 0; x < cWidth; x++) {
+      for (let y = 0; y < cHeight; y++) {
+        let value = noiseFn(
+          x / this.noiseDivider,
+          y / this.noiseDivider,
+          this.height
+        );
+        if (max < value) max = value;
+        if (min > value) min = value;
+        value = (1 + value) * 1.1 * 128;
+        var cell = (x + y * cWidth) * 4;
+        this.data[cell] = this.data[cell + 1] = this.data[cell + 2] = value;
+        //data[cell] += Math.max(0, (25 - value) * 8);
+        this.data[cell + 3] = 255; // alpha.
+      }
+    }
+
+    this.ctx.fillColor = 'black';
+    this.ctx.fillRect(0, 0, 100, 100);
+    this.ctx.putImageData(this.image, 0, 0);
+    this.height += 0.05;
+  }
+}
