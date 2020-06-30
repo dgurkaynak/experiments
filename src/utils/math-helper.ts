5e67d565765061609810dbc558bb1b45fc4d3bee
Deniz Gurkaynak <dgurkaynak@gmail.com>
2020-06-30 08:14:47 +0300

    chore: remove old utils

--- src/utils/math-helper.ts
+++
@@ -1,8 +0,0 @@
-export function lerp(A: number, B: number, ratio: number) {
-  return A + ratio * (B - A);
-}
-
-
-export function map(n: number, start1: number, stop1: number, start2: number, stop2: number) {
-  return (n - start1) / (stop1 - start1) * (stop2 - start2) + start2;
-}
